0de4bd9016c4fc4bd876fcf46d8d4a724af28b2f
Tueaj <47721101+Tueaj@users.noreply.github.com>
2020-09-09 10:13:59 +0200

    GuI lektion 4 lagt ind

---
+++ Lab1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace HullSpeed
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Sailboat boat;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            boat = new Sailboat();
+            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Keyboard.Focus(tbxName);
+        }
+
+        private void btnCalculateHullSpeed_Click(object sender, RoutedEventArgs e)
+        {
+            tbxHullSpeed.Text = boat.Hullspeed().ToString("F1");
+        }
+
+        private void tbxLength_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            boat.Length = Double.Parse(tbxLength.Text);
+        }
+
+        private void tbxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            boat.Name = tbxName.Text;
+        }
+    }
+}
---
+++ Lab2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Sailboat boat = new Sailboat();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Keyboard.Focus(tbxName);
+        }
+
+        private void btnCalculateHullSpeed_Click(object sender, RoutedEventArgs e)
+        {
+            tbxHullSpeed.Text = boat.Hullspeed().ToString("F1");
+        }
+
+        private void tbxLength_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            boat.Length = Double.Parse(tbxLength.Text);
+        }
+
+        private void tbxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            boat.Name = tbxName.Text;
+        }
+    }
+}
